73797c84377408b0ed730b0d89e217d5888e6de5
hebidu <hebiduhebi@126.com>
2014-05-07 22:27:37 +0800

    update config
    Signed-off-by: hebidu <hebiduhebi@126.com>

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -37,13 +37,13 @@ return array(
     /* 全局过滤配置 */
     'DEFAULT_FILTER' => 'safe', //全局过滤函数
 
-    /* 数据库配置 */
+    /* 数据库配置 */    
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'wp_dbwx', // 数据库名
-    'DB_USER'   => 'wp', // 用户名
-    'DB_PWD'    => '1',  // 密码
-    'DB_PORT'   => '3306', // 端口
+    'DB_HOST'   => 'sqld.duapp.com', // 服务器地址
+    'DB_NAME'   => 'OGWQmxWOuIZWNAOfiiCY', // 数据库名
+    'DB_USER'   => 'u8UAepm7wNYKh2ypXDTrn3o8', // 用户名
+    'DB_PWD'    => 'qsYrXDjv6ddG2Mh1lkurEZj1Vd9O36dN',  // 密码
+    'DB_PORT'   => '4050', // 端口
     'DB_PREFIX' => 'wp_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
