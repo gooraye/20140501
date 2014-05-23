c0038d99ffe1908306d8695790c95fc0e26281e7
hebidu <hebiduhebi@126.com>
2014-05-23 09:48:19 +0800

    update
    Signed-off-by: hebidu <hebiduhebi@126.com>

---
+++ Application/Common/Conf/test.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | Gooraye [ Freedom Work]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014 http://www.gooraye.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 贝贝 <346551990@qq.com> <http://www.gooraye.com>
+// +----------------------------------------------------------------------
+
+/**
+ * 系统配文件
+ * 所有系统级别的配置
+ */
+return array(
+    
+    /* 数据库配置 */
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'sqld.duapp.com', // 服务器地址
+    'DB_NAME'   => 'OGWQmxWOuIZWNAOfiiCY', // 数据库名
+    'DB_USER'   => 'u8UAepm7wNYKh2ypXDTrn3o8', // 用户名
+    'DB_PWD'    => 'qsYrXDjv6ddG2Mh1lkurEZj1Vd9O36dN',  // 密码
+    'DB_PORT'   => '4050', // 端口
+    'DB_PREFIX' => 'wp_', // 数据库表前缀
+    
+);
